8a84b6639a4f6c0efc7890ac36f701916f147110
Erik <78443570+thekhachik@users.noreply.github.com>
2022-03-04 15:40:40 +0300

    + html template

--- first_site/bboard/views.py
+++ first_site/bboard/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.template import loader
 from bboard.models import Bb
 
 '''
@@ -6,10 +7,16 @@ def index(request):
     return HttpResponse("Здесь будет выведен список объявлений.")
 '''
 
+'''
 def index(request):
     s = 'Список объявлений\r\n\r\n\r\n'
     for bb in Bb.objects.order_by('-published'):
         s += bb.title + '\r\n' + bb.content + '\r\n\r\n'
     return HttpResponse(s, content_type='text/plain; charset=utf-8')
+'''
 
-
+def index(request):
+    template = loader.get_template('bboard/index.html')
+    bbs = Bb.objects.order_by('-published')
+    context = {'bbs':bbs}
+    return HttpResponse(template.render(context, request))
